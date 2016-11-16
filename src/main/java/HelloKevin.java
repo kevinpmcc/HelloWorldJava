41f6a5b6e6d2c90dc8d10ec289ff07b004a9da30
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-11-16 10:42:34 +0000

    able to pass pararms to hi route and return them on screen

--- src/main/java/HelloKevin.java
+++ src/main/java/HelloKevin.java
@@ -3,9 +3,19 @@ import static spark.Spark.*;
  * Created by kevinmccarthy on 16/11/2016.
  */
 public class HelloKevin {
+
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World <a href='/bye'>Goodbye</a>");
-        get("/bye", (req, res) -> "Goodbye World");
+        get("/hello", (req, res) ->
+                "Hello World <a href='/bye'>Goodbye</a>");
+
+
+        get("/bye", (req, res) ->
+                "Goodbye World <a href='/hello'>Hello Again</a>");
+
+        get("/hi/:name", (req, res) ->
+            "Hi " + req.params(":name"));
+
     }
+
 }
 
